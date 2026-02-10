278b82d5a4fcaaf202f00419a48cab5dcd2f9b21
AnnaYakushina1328 <121216353+AnnaYakushina1328@users.noreply.github.com>
2026-02-10 20:35:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -321,11 +321,12 @@ def webhook():
         json_string = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
-…    bot.remove_webhook()
+        bot.remove_webhook()
     bot.set_webhook(url=webhook_url)
     print(f'✅ Вебхук установлен: {webhook_url}')
     
     # Запускаем Flask на порту из переменной окружения
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
+
 
